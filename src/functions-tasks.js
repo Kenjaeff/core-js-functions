cc995dc3ffc442531760c1c5ed2252120da1f182
Kenjaeff <jamol.0315.man4ik@gmail.com>
2024-04-13 21:39:58 +0500

    feat: solve 5-7 tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -91,8 +91,16 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+// 5
+function getPolynom(...coefficients) {
+  return coefficients.length === 0
+    ? null
+    : (x) =>
+        coefficients.reduce(
+          (acc, coeff, index) =>
+            acc + coeff * x ** (coefficients.length - 1 - index),
+          0
+        );
 }
 
 /**
@@ -109,8 +117,15 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+// 6
+function memoize(func) {
+  let cache;
+  return function memoizeFunc() {
+    if (cache === undefined) {
+      cache = func();
+    }
+    return cache;
+  };
 }
 
 /**
@@ -128,8 +143,20 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+// 7
+function retry(func, attempts) {
+  return function retryer() {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        if (i === attempts - 1) {
+          throw error;
+        }
+      }
+    }
+    return undefined;
+  };
 }
 
 /**
